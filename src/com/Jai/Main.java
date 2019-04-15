4618f7461cd34da21f4d21145e78e330a25d8503
Jai Lafferty <Jai.lafferty@gmail.com>
2019-04-15 18:50:19 +1000

    small fixes

--- src/com/Jai/Main.java
+++ src/com/Jai/Main.java
@@ -22,11 +22,11 @@ public class Main {
 
 		if (st == null){
 			System.out.println("ERROR: Unknown Search Type");
-			System.out.println("Available Search Types:");
+			System.out.println("Available Search Types Arguments:");
 			System.out.println("\tdepth-first");
 			System.out.println("\tbreadth-first");
 			System.out.println("\tgreedy-best");
-			System.out.println("\ta-star");
+			System.out.println("\ta-star (Not functional yet)");
 			return;
 		}
 
